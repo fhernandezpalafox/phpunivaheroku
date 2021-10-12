17f337fe447c55d3d8337c12be3556cfc89daf32
fhernandez <felipe.sistemas.fhp@gmail.com>
2021-10-12 07:19:52 -0500

    Se ha cambiando la cadena de conexion con la de la nube

--- conexionBD.php
+++ conexionBD.php
@@ -1,5 +1,6 @@
 <?php
-$mysql = new mysqli("localhost", "root", "root", "univa01");
+//$mysql = new mysqli("localhost", "root", "root", "univa01");
+$mysql = new mysqli("us-cdbr-east-04.cleardb.com", "baec3702f459d5", "57597920", "heroku_10ff34f8473daa0");
 if ($mysql->connect_error)
   die('Problemas con la conexion a la base de datos');
 
